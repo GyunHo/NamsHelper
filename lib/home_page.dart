1586e50007372501d2a3fb86ebd631dd98340952
Administrator <commandoo@hanmir.com>
2024-02-15 15:18:25 +0900

    테스트 기능

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_overlay_window/flutter_overlay_window.dart';
+import '';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +15,72 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+    double height = size.height * 0.2;
+    int overlayHeight = height.toInt();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('남스 바코드 헬퍼'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () async {
+                  final status =
+                      await FlutterOverlayWindow.isPermissionGranted();
+                  log('권한 승인 상태: $status');
+                },
+                child: const Text("권한 확인"),
+              ),
+              const SizedBox(height: 10.0),
+              TextButton(
+                onPressed: () async {
+                  final bool? res =
+                      await FlutterOverlayWindow.requestPermission();
+                  log("권한 승인 요청 결과: $res");
+                },
+                child: const Text("다른앱 위에 그리기 권한 요청"),
+              ),
+              const SizedBox(height: 10.0),
+              TextButton(
+                  onPressed: () async {
+                    final isActive = await FlutterOverlayWindow.isActive();
+                    if (isActive) return;
+                    await FlutterOverlayWindow.showOverlay(
+                        enableDrag: true,
+                        overlayTitle: '남스 헬퍼',
+                        overlayContent: '남스 헬퍼 실행중',
+                        flag: OverlayFlag.focusPointer,
+                        visibility: NotificationVisibility.visibilityPublic,
+                        positionGravity: PositionGravity.auto,
+                        height: overlayHeight,
+                        width: WindowSize.matchParent);
+                  },
+                  child: const Text('오버레이 켜기')),
+              const SizedBox(height: 10.0),
+              TextButton(
+                onPressed: () async {
+                  await FlutterOverlayWindow.closeOverlay();
+                },
+                child: const Text("오버레이 끄기"),
+              ),
+              const SizedBox(height: 10.0),
+              TextButton(
+                onPressed: () async {
+                  await Clipboard.setData(
+                      const ClipboardData(text: '클립보드 테스트'));
+                  await FlutterOverlayWindow.shareData('쉐어 데이터 테스트');
+                },
+                child: const Text("오버레이에 데이터 보내기"),
+              ),
+              const SizedBox(height: 10.0),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/overlay_page.dart
+++ lib/overlay_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_overlay_window/flutter_overlay_window.dart';
 
 class OverlayPage extends StatefulWidget {
   const OverlayPage({super.key});
@@ -10,6 +11,65 @@ class OverlayPage extends StatefulWidget {
 class _OverlayPageState extends State<OverlayPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      color: Colors.grey.withOpacity(0.1),
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 12.0),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  ListTile(
+                    leading: Container(
+                      height: 80.0,
+                      width: 80.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black54),
+                        shape: BoxShape.circle,
+                        image: const DecorationImage(
+                          image: NetworkImage(
+                              "https://api.multiavatar.com/x-slayer.png"),
+                        ),
+                      ),
+                    ),
+                    title: StreamBuilder<dynamic>(
+                        stream: FlutterOverlayWindow.overlayListener,
+                        builder: (context, snapshot) {
+                          String data = snapshot.data.toString();
+                          return Text(
+                            data,
+                            style: const TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.bold),
+                          );
+                        }),
+                    subtitle: const Text("Sousse , Tunisia"),
+                  ),
+                  const Spacer(),
+                  const Divider(color: Colors.black54),
+                ],
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: IconButton(
+                  onPressed: () async {
+                    await FlutterOverlayWindow.closeOverlay();
+                  },
+                  icon: const Icon(
+                    Icons.close,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
