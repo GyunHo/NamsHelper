8f0ea1e036b501ea44f463ef95de471a65fb627a
Administrator <commandoo@hanmir.com>
2024-02-15 14:56:28 +0900

    기본틀

---
+++ lib/overlay_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OverlayPage extends StatefulWidget {
+  const OverlayPage({super.key});
+
+  @override
+  State<OverlayPage> createState() => _OverlayPageState();
+}
+
+class _OverlayPageState extends State<OverlayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
