e0d80444dddf22b13261325394bccfcd6a76a0ad
Administrator <commandoo@hanmir.com>
2024-03-11 15:37:14 +0900

    disable back button for exit app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,6 +17,10 @@ class _HomePageState extends State<HomePage> {
   TextEditingController textEditingController = TextEditingController();
   final _receivePort = ReceivePort();
 
+  DateTime? currentBackPressTime;
+  int requiredMilliSeconds = 500;
+  bool canPop = false;
+
   @override
   void initState() {
     super.initState();
@@ -47,81 +51,86 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Ctrl CV'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Flexible(
-              child: TextField(
-                controller: textEditingController,
-                maxLines: null, // Set this
-                expands: true, // and this
-                keyboardType: TextInputType.multiline,
+      body: PopScope(
+        canPop: false,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Flexible(
+                child: TextField(
+                  controller: textEditingController,
+                  maxLines: null, // Set this
+                  expands: true, // and this
+                  keyboardType: TextInputType.multiline,
+                ),
               ),
-            ),
-            SingleChildScrollView(
-              child: Row(
-                children: [
-                  TextButton(
-                    onPressed: () async {
-                      final status =
-                          await FlutterOverlayWindow.isPermissionGranted();
-                      log('권한 승인 상태: $status');
-                      if (!mounted) return;
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          duration: const Duration(seconds: 1),
-                          content:
-                              Text(status ? "권한 승인 상태" : '권한 거부 상태, 권한 부여필요')));
-                    },
-                    child: const Text("권한 확인"),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      final bool? res =
-                          await FlutterOverlayWindow.requestPermission();
-                      log("권한 승인 요청 결과: $res");
-                    },
-                    child: const Text("권한 요청"),
-                  ),
-                  TextButton(
+              SingleChildScrollView(
+                child: Row(
+                  children: [
+                    TextButton(
                       onPressed: () async {
-                        final isActive = await FlutterOverlayWindow.isActive();
-                        if (isActive) return;
-                        await FlutterOverlayWindow.showOverlay(
-                            alignment: OverlayAlignment.topCenter,
-                            enableDrag: false,
-                            overlayTitle: 'PASTE HELPER',
-                            overlayContent: 'helper running',
-                            flag: OverlayFlag.defaultFlag,
-                            visibility: NotificationVisibility.visibilityPublic,
-                            positionGravity: PositionGravity.none,
-                            height: 500,
-                            width: WindowSize.matchParent);
+                        final status =
+                            await FlutterOverlayWindow.isPermissionGranted();
+                        log('권한 승인 상태: $status');
+                        if (!mounted) return;
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            duration: const Duration(seconds: 1),
+                            content: Text(
+                                status ? "권한 승인 상태" : '권한 거부 상태, 권한 부여필요')));
                       },
-                      child: const Text('오버레이 켜기')),
-                  TextButton(
-                    onPressed: () async {
-                      await FlutterOverlayWindow.closeOverlay();
-                    },
-                    child: const Text("오버레이 끄기"),
-                  ),
-                ],
+                      child: const Text("권한 확인"),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final bool? res =
+                            await FlutterOverlayWindow.requestPermission();
+                        log("권한 승인 요청 결과: $res");
+                      },
+                      child: const Text("권한 요청"),
+                    ),
+                    TextButton(
+                        onPressed: () async {
+                          final isActive =
+                              await FlutterOverlayWindow.isActive();
+                          if (isActive) return;
+                          await FlutterOverlayWindow.showOverlay(
+                              alignment: OverlayAlignment.topCenter,
+                              enableDrag: false,
+                              overlayTitle: 'PASTE HELPER',
+                              overlayContent: 'helper running',
+                              flag: OverlayFlag.defaultFlag,
+                              visibility:
+                                  NotificationVisibility.visibilityPublic,
+                              positionGravity: PositionGravity.none,
+                              height: 500,
+                              width: WindowSize.matchParent);
+                        },
+                        child: const Text('오버레이 켜기')),
+                    TextButton(
+                      onPressed: () async {
+                        await FlutterOverlayWindow.closeOverlay();
+                      },
+                      child: const Text("오버레이 끄기"),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const Divider(),
-            TextButton(
-              onPressed: () async {
-                List<List<String>> data = splitText();
-                if (await FlutterOverlayWindow.isPermissionGranted()) {
-                  await FlutterOverlayWindow.shareData(data);
-                }
-                setState(() {
-                  textEditingController.clear();
-                });
-              },
-              child: const Text("오버레이에 데이터 보내기"),
-            ),
-          ],
+              const Divider(),
+              TextButton(
+                onPressed: () async {
+                  List<List<String>> data = splitText();
+                  if (await FlutterOverlayWindow.isPermissionGranted()) {
+                    await FlutterOverlayWindow.shareData(data);
+                  }
+                  setState(() {
+                    textEditingController.clear();
+                  });
+                },
+                child: const Text("오버레이에 데이터 보내기"),
+              ),
+            ],
+          ),
         ),
       ),
     );
